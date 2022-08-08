12a0954391995ff9fdc43fdf081957a787f0d855
s18982@pjwstk.edu.pl <s18982@pjwstk.edu.pl>
2022-08-08 11:30:09 +0200

    utworzenie projektu apki

---
+++ StudentApplication/StudentApplication/Controllers/StartController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentApplication.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class StartController : ControllerBase
+    {
+        [HttpGet]
+        public string Start()
+        {
+            return "Hello! It's home page";
+        }
+    }
+}
---
+++ StudentApplication/StudentApplication/Controllers/StudentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentApplication.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok("okejo");
+        }
+    }
+}
